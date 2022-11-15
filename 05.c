e538ada27e7c15a3447b116f4a633ec49d7ec947
renanguillen <rigcpj@gmail.com>
2022-11-15 08:42:30 -0300

    05 file

---
+++ 05.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LIN_MAX 3
+#define COL_MAX 5
+
+static void	free_slot(int **slot)
+{
+	int	i;
+
+	i = 0;
+	if (slot)
+	{
+		while (i < 3)
+			free(slot[i++]);
+		free(slot);
+	}
+	return ;
+}
+
+static int	**slot_in(void)
+{
+	int	i;
+	int	j;
+	int	**slot;
+
+	i = 0;
+	slot = malloc(sizeof(int *) * LIN_MAX);
+	while (i < LIN_MAX)
+		slot[i++] = malloc(sizeof(int) * COL_MAX);
+	i = 0;
+	while (i < LIN_MAX)
+	{
+		j = 0;
+		printf("Insira os valores da linha %d:\n", (i + 1));
+		while (j < COL_MAX)
+			scanf("%d", &slot[i][j++]);
+		i++;
+	}
+	return (slot);
+}
+
+static int	check(int **slot, int prize[LIN_MAX][COL_MAX])
+{
+	int	counter;
+	int	j;
+	int	i;
+
+	i = 0;
+	counter = 0;
+	while (i < LIN_MAX)
+	{
+		j = 0;
+		while (j < COL_MAX)
+		{
+			if (slot[i][j] == prize[i][j])
+				counter++;
+			j++;
+		}
+		i++;
+	}
+	return (counter);
+}
+
+int	main(void)
+{
+	int	prize[LIN_MAX][COL_MAX] = {{1, 0, 0, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 0}};
+	int	**slot;
+
+	slot = NULL;
+	while (1)
+	{
+		free_slot(slot);
+		slot = slot_in();
+		if (check(slot, prize) == (LIN_MAX * COL_MAX))
+		{
+			printf("Ganhou!\n");
+			break ;
+		}
+		else
+			printf("Tente Novamente\n");
+	}
+	return (0);
+}
--- Makefile
+++ Makefile
@@ -16,6 +16,10 @@ Q04 = 04
 04_SRCS = 04.c
 04_OBJS = 04.o
 
+Q05 = 05
+05_SRCS = 05.c
+05_OBJS = 05.o
+
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror -g
 IFLAGS = -I .
@@ -24,18 +28,18 @@ LIBFT = ./libft/libft.a
 
 all: $(NAME)
 
-$(NAME): $(LIBFT) $(Q01) $(Q02) $(Q03)
+$(NAME): $(LIBFT) $(Q01) $(Q02) $(Q03) $(Q05)
 
 $(LIBFT):
 	@make -C ./libft
 
 $(Q01): $(01_OBJS)
-	$(CC) $(CFLAGS) $(IFLAGS) -o $(Q01) $(01_OBJS) $(LIBFT) -lm
+	$(CC) $(CFLAGS) $(IFLAGS) -o $(Q01) $(01_OBJS) $(LIBFT)
 $(01_OBJS): $(01_SRCS)
 	$(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@
 
 $(Q02): $(02_OBJS)
-	$(CC) $(CFLAGS) $(IFLAGS) -o $(Q02) $(02_OBJS) $(LIBFT) -lm
+	$(CC) $(CFLAGS) $(IFLAGS) -o $(Q02) $(02_OBJS) $(LIBFT)
 $(02_OBJS): $(02_SRCS)
 	$(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@
 
@@ -44,12 +48,17 @@ $(Q03): $(03_OBJS)
 $(03_OBJS): $(03_SRCS)
 	$(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@
 
+$(Q05): $(05_OBJS)
+	$(CC) $(CFLAGS) -o $(Q05) $(05_OBJS)
+$(05_OBJS): $(05_SRCS)
+	$(CC) $(CFLAGS) -c $< -o $@
+
 clean:
-	@rm -rf $(01_OBJS) $(02_OBJS) $(03_OBJS) $(04_OBJS)
+	@rm -rf $(01_OBJS) $(02_OBJS) $(03_OBJS) $(04_OBJS) $(05_OBJS)
 
 fclean: clean
 	@make fclean -C ./libft
-	@rm -rf $(Q01) $(Q02) $(Q03) $(Q04)
+	@rm -rf $(Q01) $(Q02) $(Q03) $(Q04) $(Q05)
 
 re: fclean all
 
